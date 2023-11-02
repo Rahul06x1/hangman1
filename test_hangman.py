f6595d6c15ee3b5823763679e35a8bc135488e83
Rahul Rajeev <rahulrajeev2k1@gmail.com>
2023-11-02 14:04:19 +0530

    Implement function test_random_word_lowercase [ Red: Lowercase letters only ]

---
+++ test_hangman.py
@@ -0,0 +1,13 @@
+import os
+from hangman import *
+
+def test_random_word_lowercase():
+    fname = "/tmp/sample_wordlist"
+    with open(fname, "w") as f:
+        f.writelines(["Grape\n", "apple\n", "Mango\n"])
+        
+    for _ in range(100):
+        assert get_random_word(fname) == "apple"
+
+    os.unlink(fname)
+    
